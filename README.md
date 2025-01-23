e1496da44c92ab4107a3e91ced6f716586281e4d
Dubnium32x <63617522+Dubnium32x@users.noreply.github.com>
2025-01-22 20:36:20 -0800

    made edits of my own

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # cswlib
 ## A rewrite of cwlib in C#
 
-Uhh this sucks rn but im sleepy ill work on the readme later
+This does NOT recreate the entirety of Craftworld Toolkit. Rather it is a replica of the library files found in the toolkit
+software, as well as files that were once used in LBP itself, but now in C#.
+
+This will allow others, including but not limited to Craftworld HUB, to be able to use LBP libraries within Unity and Godot
+at their leisure. Please go support the original authors of the code.
+
+C# Code written by
+: DiSKO
+: FeTetra
 
 Original library by ennuo:
 https://github.com/ennuo/toolkit/tree/main/lib/cwlib
---
+++ cswlib/cswlib/src/main/types/data/Revision.cs
@@ -0,0 +1,166 @@
+package cwlib.types.data;
+
+import cwlib.enums.Branch;
+
+/**
+ * Utilities for comparing game revisions.
+ */
+public readonly class Revision
+{
+    public static readonly int LBP1_readonly_REVISION = 0x272;
+
+    private readonly int head;
+    private readonly short branchID;
+    private readonly short branchRevision;
+
+    /**
+     * Forms revision with branch data.
+     *
+     * @param revision          Head revision
+     * @param branchDescription Revision branch descriptor
+     */
+    public Revision(int revision, int branchDescription)
+    {
+        this.head = revision;
+        this.branchID = (short) (ref branchDescription >> 0x10);
+        this.branchRevision = (short) (ref branchDescription & 0xFFFF);
+    }
+
+    /**
+     * Forms revision with no branch data.
+     *
+     * @param revision Head revision
+     */
+    public Revision(int revision)
+    {
+        this.head = revision;
+        this.branchID = 0;
+        this.branchRevision = 0;
+    }
+
+    /**
+     * Forms revision with branch ID and revision.
+     *
+     * @param revision       Head revision
+     * @param branchID       Branch ID of revision
+     * @param branchRevision Revision of branch
+     */
+    public Revision(int revision, int branchID, int branchRevision)
+    {
+        this.head = revision;
+        this.branchID = (short) branchID;
+        this.branchRevision = (short) branchRevision;
+    }
+
+    public bool isLBP1()
+    {
+        return this.head <= Revision.LBP1_readonly_REVISION;
+    }
+
+    public bool isLBP2()
+    {
+        return !this.isLBP1() && !this.isLBP3() && !this.isVita();
+    }
+
+    public bool isLBP3()
+    {
+        return this.head >> 0x10 != 0;
+    }
+
+    public bool isLeerdammer()
+    {
+        return this.is(Branch.LEERDAMMER);
+    }
+
+    public bool isVita()
+    {
+        int version = this.getVersion();
+        return this.branchID == Branch.DOUBLE11.getID() && version >= 0x3c1 && version <= 0x3e2;
+    }
+
+    public bool isToolkit()
+    {
+        return this.is(Branch.MIZUKI);
+    }
+
+    public bool is(Branch branch)
+    {
+        if (branch == Branch.DOUBLE11) return this.isVita();
+        return this.branchID == branch.getID() && this.head == branch.getHead();
+    }
+
+    public bool has(Branch branch, int revision)
+    {
+        if (!this.is(branch)) return false;
+        return this.branchRevision >= revision;
+    }
+
+    public bool after(Branch branch, int revision)
+    {
+        if (!this.is(branch)) return false;
+        return this.branchRevision > revision;
+    }
+
+    public bool before(Branch branch, int revision)
+    {
+        if (!this.is(branch)) return false;
+        return this.branchRevision < revision;
+    }
+
+    /**
+     * Gets the LBP3 specific revision of the head revision.
+     *
+     * @return LBP3 head revision
+     */
+    public int getSubVersion()
+    {
+        return (this.head >>> 16) & 0xFFFF;
+    }
+
+    /**
+     * Gets the LBP1/LBP2/V revision of the head revision.
+     *
+     * @return LBP1/2/V head revision
+     */
+    public int getVersion()
+    {
+        return this.head & 0xFFFF;
+    }
+
+    public int getHead()
+    {
+        return this.head;
+    }
+
+    public short getBranchID()
+    {
+        return this.branchID;
+    }
+
+    public short getBranchRevision()
+    {
+        return this.branchRevision;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        if (this.branchID != 0)
+        {
+            return String.format("Revision: (r%d, b%04X:%04X)",
+                this.head, this.branchID, this.branchRevision);
+        }
+        return String.format("Revision: (r%d)", this.head);
+    }
+
+    @Override
+    public bool equals(Object other)
+    {
+        if (other == this) return true;
+        if (!(other instanceof Revision otherRevision)) return false;
+        return otherRevision.head == this.head
+               && otherRevision.branchID == this.branchID
+               && otherRevision.branchRevision == this.branchRevision;
+    }
+}
